d3de9f8c8b90aa0c31810ab4763aa70995c8acd9
araybr <araybrito2005@gmail.com>
2025-10-15 13:47:41 +0200

    Cambio carrusel pagina principal. Home.ts añado los scripts y pongo el encapsulation.none para que se apliquen los estilos del carrusel sin problemas

--- FRONT/restaurante/src/app/pages/home/home.ts
+++ FRONT/restaurante/src/app/pages/home/home.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewEncapsulation } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
-  styleUrls: ['./home.css']
+  styleUrls: ['./home.css'],
+  encapsulation: ViewEncapsulation.None // <-- esto hace que los CSS sean globales
 })
-export class Home {
 
+export class Home implements AfterViewInit  {
+  ngAfterViewInit(): void {
+    this.loadScript('assets/js/modernizer.js')
+      .then(() => this.loadScript('assets/js/bootstrap.min.js'))
+      .then(() => this.loadScript('assets/js/all.js'))
+      .then(() => this.loadScript('assets/js/custom.js'))
+      .then(() => {
+        // Inicializar Owl Carousel
+        ($('#owl-demo') as any).owlCarousel({
+          autoPlay: 3000,
+          items: 3,
+          itemsDesktop: [1199, 3],
+          itemsDesktopSmall: [979, 2]
+        });
+      })
+      .catch(err => console.error(err));
+  }
+
+  private loadScript(url: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = url;
+      script.async = true;
+      script.onload = () => resolve();
+      script.onerror = () => reject(`No se pudo cargar ${url}`);
+      document.body.appendChild(script);
+    });
+  }
 }
