d8c68a1eb066d194efdab0938d241a2a9601b406
araybr <araybrito2005@gmail.com>
2025-10-15 15:34:22 +0200

    Apartado menus pagina principal Slick

--- FRONT/restaurante/src/app/pages/home/home.ts
+++ FRONT/restaurante/src/app/pages/home/home.ts
@@ -16,16 +16,59 @@ export class Home implements AfterViewInit  {
       .then(() => this.loadScript('assets/js/all.js'))
       .then(() => this.loadScript('assets/js/custom.js'))
       .then(() => {
-        // Inicializar Owl Carousel
+        // Carrusel
         ($('#owl-demo') as any).owlCarousel({
           autoPlay: 3000,
           items: 3,
           itemsDesktop: [1199, 3],
           itemsDesktopSmall: [979, 2]
         });
+
+        // Slick Slider
+        ($('.slider-single') as any).slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          fade: false,
+          adaptiveHeight: true,
+          infinite: false,
+          speed: 400,
+          cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)'
+        });
+
+        ($('.slider-nav') as any)
+          .on('init', (event: any, slick: any) => {
+            $('.slider-nav .slick-slide.slick-current').addClass('is-active');
+          })
+          .slick({
+            slidesToShow: 4,
+            slidesToScroll: 7,
+            dots: false,
+            focusOnSelect: false,
+            infinite: false,
+            responsive: [
+              { breakpoint: 1024, settings: { slidesToShow: 4, slidesToScroll: 4 } },
+              { breakpoint: 769, settings: { slidesToShow: 4, slidesToScroll: 4 } },
+              { breakpoint: 420, settings: { slidesToShow: 3, slidesToScroll: 3 } }
+            ]
+          });
+
+        ($('.slider-single') as any).on('afterChange', (event: any, slick: any, currentSlide: number) => {
+          ($('.slider-nav') as any).slick('slickGoTo', currentSlide);
+          const curSlide = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
+          $('.slider-nav .slick-slide.is-active').removeClass('is-active');
+          $(curSlide).addClass('is-active');
+        });
+
+        ($('.slider-nav') as any).on('click', '.slick-slide', (event: any) => {
+          event.preventDefault();
+          const goToSingleSlide = $(event.currentTarget).data('slick-index');
+          ($('.slider-single') as any).slick('slickGoTo', goToSingleSlide);
+        });
       })
       .catch(err => console.error(err));
   }
+
 
   private loadScript(url: string): Promise<void> {
     return new Promise((resolve, reject) => {
